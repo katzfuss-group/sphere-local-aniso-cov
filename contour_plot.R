b22fb72f3c9d9dea42abd4f815ceb989ddca8a99
Jian Cao <jian.cao@kaust.edu.sa>
2023-04-27 09:39:40 -0500

    Contour plots for three kernels

--- contour_plot.R
+++ contour_plot.R
@@ -4,9 +4,15 @@ devtools::install_github("https://github.com/katzfuss-group/GPvecchia",
 )
 source("func.R")
 
-plot_contour <- function(par, locs, level = 0.1) {
+plot_contour <- function(par, locs, level = 0.7, fn = NULL) {
   n_locs <- nrow(locs)
   contours <- list()
+  if (!is.null(fn)) {
+    fig.width <- 13.44
+    fig.height <- 8.73
+    pdf(fn, fig.width, fig.height)
+  }
+
   mapPlot(
     longitude = c(-180, 180, 0, 0), latitude = c(0, 0, -90, 90),
     grid = TRUE, col = "lightgray", drawBox = FALSE,
@@ -29,6 +35,16 @@ plot_contour <- function(par, locs, level = 0.1) {
     cov_mat <- cov_gen_row1(grid, alpha, beta, kappa, nu, range, 0)
     idx <- cov_mat[1, ] < level + 0.01 & cov_mat[1, ] > level - 0.01
     if (sum(idx) == 0) {
+      lon_range <- seq(from = lon - 0.1, to = lon + 0.1, length.out = 100)
+      lat_range <- seq(from = lat - 0.1, to = lat + 0.1, length.out = 100)
+      lon_range <- lon_range[lon_range < pi & lon_range > -pi]
+      lat_range <- lat_range[lat_range < pi / 2 & lat_range > -pi / 2]
+      grid <- as.matrix(expand.grid(lon_range, lat_range))
+      grid <- rbind(locs[i, , drop = F], grid)
+      cov_mat <- cov_gen_row1(grid, alpha, beta, kappa, nu, range, 0)
+      idx <- cov_mat[1, ] < level + 0.02 & cov_mat[1, ] > level - 0.02
+    }
+    if (sum(idx) <= 1) {
       stop(cat("Cannot find contour of level", level))
     }
     grid_sub <- grid[idx, , drop = F]
@@ -47,8 +63,11 @@ plot_contour <- function(par, locs, level = 0.1) {
       }
     }
     grid_sub_tmp <- rbind(grid_sub_tmp, grid_sub_tmp[1, ])
-    mapPoints(lon * 180 / pi, lat * 180 / pi, col = "red", pch = 16)
+    # mapPoints(lon * 180 / pi, lat * 180 / pi, col = "red", pch = 16)
     mapLines(grid_sub_tmp[, 1] * 180 / pi, grid_sub_tmp[, 2] * 180 / pi, col = "red")
+  }
+  if (!is.null(fn)) {
+    dev.off()
   }
 }
 
@@ -114,7 +133,20 @@ cov_gen_row1 <- function(grd.all, alpha, beta, kappa, nu, range, nuggets,
 }
 locs <- as.matrix(expand.grid(
   c(-2:2) * pi / 3,
-  c(-1:1) * pi / 4
+  c(-3:3) * pi / 8
 ))
+
 par_iso <- c(8.476336, 0, 0, 8.476336, 0, 0, 0, 2.5, exp(0.07437005))
-plot_contour(par_iso, locs)
+par_axial <- c(
+  -2.628519, 0, -0.6661641, -6.35629, 0, -0.2518264, 0, 2.5,
+  exp(0.07437005)
+)
+par_nonsta <- c(
+  -2.441852, -0.0654426, -0.7878552, -6.375738, -0.2483788,
+  -0.2730235, -6.32839, 2.5, exp(0.07437005)
+)
+for (type in c("iso", "axial", "nonsta"))
+# for(type in c("iso"))
+{
+  plot_contour(get(paste0("par_", type)), locs, fn = paste0("contour_", type, ".pdf"))
+}
